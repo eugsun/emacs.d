55a47d64a26f8674eeeeea675156fda8693a33c6
Eugene Y. Sun <eug.sun@gmail.com>
2013-03-03 13:04:33 -0500

    make evince the default pdf viewer

--- init-auctex-config.el
+++ init-auctex-config.el
@@ -18,3 +18,19 @@
 
 ;; set up auto-complete for auctux
 (require 'auto-complete-auctex)
+
+;; add PDFLatex to the command-list
+(add-hook 'LaTeX-mode-hook 'pdflatex-mode-hook)
+(defun pdflatex-mode-hook ()
+  (add-to-list 'TeX-command-list
+	       '("PDFLaTeX" "%'pdflatex%(mode)%' %t" TeX-run-TeX nil t)))
+
+;; add Make to the command-list
+(add-hook 'LaTeX-mode-hook 'make-mode-hook)
+(defun make-mode-hook ()
+  (add-to-list 'TeX-command-list
+	       '("Make" "%'make all%'" TeX-run-TeX nil t)))
+
+;; set evince to be the default view program
+(custom-set-variables
+ '(TeX-view-program-selection (quote (((output-dvi style-pstricks) "dvips and gv") (output-dvi "xdvi") (output-pdf "Evince") (output-html "xdg-open")))))
