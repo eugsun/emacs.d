7ba4eefdc0aab7e0e3ef6b5d51124624f97b86a5
Eugene Sun <eug.sun@gmail.com>
2021-02-05 20:48:55 -0500

    Add vue support

--- custom/init-web.el
+++ custom/init-web.el
@@ -1,7 +1,7 @@
 ;; -*- lexical-binding: t; -*-
 
 (use-package web-mode
-  :mode ("\\.html\\'" "\\.css\\'")
+  :mode ("\\.html\\'" "\\.css\\'" "\\.vue\\'")
   :config
   (setq web-mode-markup-indent-offset 2)
   (setq web-mode-css-indent-offset 2)
